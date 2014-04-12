25b6b8d8a1b3c3e667ce3990a71c6a65bd736b1b
James Kozianski <koz@chromium.org>
2014-04-13 08:43:01 +1000

    Set our own GDT.

--- start.s
+++ start.s
@@ -27,9 +27,48 @@ align 4
 extern kmain
 
 start:
+  mov ax, 0x10
+  mov ds, ax
+  mov ss, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  lgdt [GDT_toc]
   mov esp, stack_top
-  mov [0xb8000], byte 67
   push ebx
   push eax
   call kmain
   jmp $
+
+GDT_start:
+  ; null selector (reserved by processor)
+  dd 0
+  dd 0
+
+  ; code selector
+  dw 0xffff     ; Limit 0-15
+  dw 0x0        ; Base 0-15
+  db 0x0        ; Base 16-23
+  db 10011010b  ; See http://www.brokenthorn.com/Resources/OSDev8.html
+  db 11001111b  ; As above.
+  db 0x0        ; Base 24-31
+
+  ; data selector - as above but with the code/data bit set to 0
+  dw 0xffff
+  dw 0x0
+  db 0x0
+  db 10010010b  ; bit 3 here is 0, not 1 as above
+  db 11001111b
+  db 0x0
+
+  ; null selector (reserved by processor)
+  dd 0
+  dd 0
+  ; null selector (reserved by processor)
+  dd 0
+  dd 0
+GDT_end:
+
+GDT_toc:
+  dw GDT_end - GDT_start - 1    ; size of GDT - 1
+  dd GDT_start
