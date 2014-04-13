f25eafad807a26b36dfe92eb89f78f6397593ff4
James Kozianski <koz@chromium.org>
2014-04-14 08:30:18 +1000

    Nits.

--- interrupt.s
+++ interrupt.s
@@ -9,31 +9,31 @@ global SetupPic
 ; to the same interrupt handlers, so we remap the interrupts here from
 ; 0x0..0xf -> 0x20..0x2f.
 SetupPic:
-  ; ICW to both
+  ; ICW to both.
   mov al, 0x11
   out PIC1, al
   out PIC2, al
 
-  ; Set the IRQ bases - 32 and 40 respectively
+  ; Set the IRQ bases - 0x20 and 0x28 respectively.
   mov al, 0x20
   out PIC1CTL, al
   mov al, 0x28
   out PIC2CTL, al
 
-  ; PIC1's slave address in unary (ie: 2)
+  ; PIC1's slave address in unary (ie: 2).
   mov al, 0x4
   out PIC1CTL, al
 
-  ; PIC2's master address in binary
+  ; PIC2's master address in binary.
   mov al, 0x2
   out PIC2CTL, al
 
-  ; 8086 mode for both
+  ; 8086 mode for both.
   mov al, 0x1
   out PIC1CTL, al
   out PIC2CTL, al
 
-  ; mask on all interrupts
+  ; Mask on all interrupts.
   mov al, 0x0
   out PIC1CTL, al
   out PIC2CTL, al
@@ -129,4 +129,3 @@ ISR_NOERRCODE 28
 ISR_NOERRCODE 29
 ISR_NOERRCODE 30
 ISR_NOERRCODE 31
-
